2884ef05729e9022e4c327ad4b0268f9c10bd642
SteniusJ <stenius.patrik@gmail.com>
2025-10-26 19:59:40 +0200

    add file write to disk from cache

--- src/file.rs
+++ src/file.rs
@@ -5,9 +5,7 @@ use std::io::Result;
 
 #[derive(Debug)]
 pub enum Status {
-    Success,
-    FileInCache,
-    WriteSuccess
+    Success
 }
 
 pub struct FileSystem {
@@ -28,22 +26,21 @@ impl FileSystem {
     /// If file is in cache file is not opened again
     /// If file doesn't exist it is created
     pub fn open(&mut self, file_name: &str) -> Result<Status> {
-
-        if self.cache.contains_key(file_name) {
-            return Ok(Status::FileInCache)
+        if self.is_in_cache(file_name) {
+            return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "File already in cache"));
         }
 
         let mut file: File;
 
-        match File::open("../tables/".to_string() + file_name) {
+        match File::open(file_name) {
             Ok(f) => file = f,
             Err(_) => {
-                match File::create("../tables/".to_string() + file_name) {
+                match File::create(file_name) {
                     Ok(_) => println!("New file created"),
                     Err(e) => println!("{e:?}"),
                 }
 
-                file = File::open("../tables/".to_string() + file_name)?;
+                file = File::open(file_name)?;
             },
         }
 
@@ -56,20 +53,18 @@ impl FileSystem {
     }
 
     pub fn write_to_cache(&mut self, file_name: &str, new_content: String) -> Result<Status> {
-
-        if !self.cache.contains_key(file_name) {
-            return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "File by name {file_name:?} is not in cache"))
+        if !self.is_in_cache(file_name) {
+            return Err(std::io::Error::new(std::io::ErrorKind::NotFound, "File not in cache"))
         }
 
         self.add_to_cache(file_name.to_string(), new_content);
 
-        Ok(Status::WriteSuccess)
+        Ok(Status::Success)
     }
 
     pub fn read_from_cache(&mut self, file_name: &str) -> Result<String> {
-
-        if !self.cache.contains_key(file_name) {
-            return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "File by name {file_name:?} is not in cache"))
+        if !self.is_in_cache(file_name) {
+            return Err(std::io::Error::new(std::io::ErrorKind::NotFound, "File not in cache"))
         }
 
         let contents = String::from(self.cache.get(file_name).unwrap());
@@ -77,7 +72,40 @@ impl FileSystem {
         Ok(contents)
     }
 
+    pub fn write_cache_to_disk(&mut self, file_name: &str) -> Result<Status> {
+        if !self.is_in_cache(file_name) {
+            return Err(std::io::Error::new(std::io::ErrorKind::NotFound, "File not in cache"))
+        }
+
+        let mut file: File;
+
+        match File::create(file_name) {
+            Ok(f) => file = f,
+            Err(e) => return Err(e),
+        }
+
+        let contents = String::from(self.cache.get(file_name).unwrap());
+
+        println!("File contents: {contents}");
+
+        match file.write_all(contents.as_bytes()) {
+            Ok(_) => {
+                self.cache.remove(file_name);
+
+                return Ok(Status::Success)
+            },
+            Err(e) => return Err(e),
+        }
+    }
+
     fn add_to_cache(&mut self, file_name: String, file_contents: String) {
         self.cache.insert(file_name, file_contents);
+    }
+
+    fn is_in_cache(&mut self, file_name: &str) -> bool {
+        if self.cache.contains_key(file_name) {
+            return true
+        }
+        false
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
 mod file;
 
+const TEST_FILE_PATH: &str = "./tables/test";
+
 fn main() {
     let mut file_system = file::FileSystem::new();
 
-     match file_system.open("test") {
-         Ok(status) => println!("file opened successfully: {status:?}"),
+     match file_system.open(TEST_FILE_PATH) {
+         Ok(status) => println!("File open status: {status:?}"),
          Err(e) => println!("file open failed: {e:?}"),
      }
 
-     match file_system.read_from_cache("test") {
-         Ok(contents) => println!("{contents}"),
+     match file_system.read_from_cache(TEST_FILE_PATH) {
+         Ok(contents) => println!("Read return: {contents}"),
          Err(e) => println!("Read failed: {e:?}"),
      }
 
-     match file_system.write_to_cache("test", "New content in cache".to_string()) {
-         Ok(s) => println!("Status: {s:?}"),
-         Err(e) => println!("{e:?}"),
+     match file_system.write_to_cache(TEST_FILE_PATH, "New content in cache".to_string()) {
+         Ok(s) => println!("Write status: {s:?}"),
+         Err(e) => println!("Write fail: {e:?}"),
      }
 
-     match file_system.read_from_cache("test") {
-         Ok(contents) => println!("{contents}"),
+     match file_system.read_from_cache(TEST_FILE_PATH) {
+         Ok(contents) => println!("Read return: {contents}"),
          Err(e) => println!("Read failed: {e:?}"),
      }
 
-     match file_system.open("test") {
-         Ok(status) => println!("file opened for a second time: {status:?}"),
+     match file_system.open(TEST_FILE_PATH) {
+         Ok(status) => println!("File open status: {status:?}"),
          Err(e) => println!("file open failed: {e:?}"),
+     }
+
+     match file_system.write_cache_to_disk(TEST_FILE_PATH) {
+         Ok(s) => println!("File disk write status: {s:?}"),
+         Err(e) => println!("File open failed: {e:?}"),
      }
 }
