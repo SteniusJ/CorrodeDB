db2664c8ae149507a2a739c5c97729b2f97c1a0e
SteniusJ <stenius.patrik@gmail.com>
2026-01-27 18:27:26 +0200

    add support for arrays and objects to json encoder

--- src/json/mod.rs
+++ src/json/mod.rs
@@ -1,7 +1,45 @@
+use std::fmt;
+
 pub enum JSONValue {
     String(String),
     NumI(i64),
     NumDec(f64),
+    Array(Vec<JSONValue>),
+    Object(Vec<(String, JSONValue)>),
+}
+
+impl fmt::Display for JSONValue {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            JSONValue::String(v) => {
+                write!(f, "{}", v)
+            },
+            JSONValue::NumI(v) => {
+                write!(f, "{}", v)
+            },
+            JSONValue::NumDec(v) => {
+                write!(f, "{}", v)
+            },
+            JSONValue::Array(v) => {
+                let mut ret = String::from("[");
+                for val in v {
+                    ret.push_str(format!("{},", val).as_str());
+                }
+                ret.pop();
+                ret.push(']');
+                write!(f, "{}", ret)
+            },
+            JSONValue::Object(v) => {
+                let mut ret = String::from("{");
+                for val in v {
+                    ret.push_str(format!("{}:{},", val.0, val.1).as_str());
+                }
+                ret.pop();
+                ret.push('}');
+                write!(f, "{}", ret)
+            }
+        }
+    }
 }
 
 // Simple JSON encoder that fits this projects needs
@@ -13,12 +51,9 @@ pub fn encode(values: Vec<(&str, JSONValue)>) -> String {
             JSONValue::String(v) => {
                 json_string.push_str(format!("\"{}\":\"{}\",", val.0, v).as_str());
             },
-            JSONValue::NumI(v) => {
-                json_string.push_str(format!("\"{}\":{},", val.0, v).as_str());
-            },
-            JSONValue::NumDec(v) => {
-                json_string.push_str(format!("\"{}\":{},", val.0, v).as_str());
-            },
+            _ => {
+                json_string.push_str(format!("\"{}\":{},", val.0, val.1).as_str());
+            }
         }
     }
 
